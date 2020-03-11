bad2365c8ebe9f90e325cd59da624f256997bb6d
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2020-03-11 17:08:40 +0200

    Update tasks.lisp

--- tasks.lisp
+++ tasks.lisp
@@ -3,12 +3,23 @@
 ; Oпределите функцию, которая увеличивает элементы исходного списка на единицу.
 
 (defun inc-list (lst) (
-cond 
-(( null lst) NIL)
-(t (cons (+ (car lst) 1) (inc-list(cdr lst)))
-)
-)
-)
+                       cond ((null lst) NIL)
+                       (t (cons (+ (car lst) 1) 
+                                (inc-list(cdr lst)))
+                          )
+                       )
+  )
 
-(inc-list(1 2 3))
+(inc-list '(1 2 3))
 
+; Oпределите функцию, удаляющую из исходного списка элементы с четными номерами
+
+(defun del-even (lst) (
+                       cond ((null lst) NIL)
+                       (t (cons (car lst)
+                                (del-even(cddr lst)))
+                          )
+                       )
+  )
+
+(del-even '(1 2 3 4 5 6))
