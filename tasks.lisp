2deb697545fe92d0de532543d3796e9adb777bd2
temirkayaeva <40575841+temirkayaeva@users.noreply.github.com>
2020-04-08 14:54:40 +0300

    Update tasks.lisp

--- tasks.lisp
+++ tasks.lisp
@@ -1,5 +1,42 @@
 ; задачи 15 5 7 11 12 13 19 31 41 47 48
 
+; 12:  Определите функцию, заменяющую в исходном списке два подряд идущих одинаковых элемента одним
+
+(defun task (lst)
+    ((lambda (первый)
+         (cond
+            ((null lst)
+                 NIL)
+            ((equal первый (cadr lst))
+                 (cons первый (task (cddr lst))))
+            (t
+                 (cons первый (task (cdr lst))))
+         )) (car lst))  ; первый = (car lst)
+)
+
+
+(print(task '(1 1 1 2 2 2))) 
+(print(task '(1 2 3)))
+(print(task '(2 2 2 2 2 2 2 3))) 
+
+; 11. Определите функцию, осуществляющую разделение исходного списка на два подсписка. В первый из них должно попасть указанное количество элементов с начала списка, во второй — оставшиеся элементы.
+
+(defun task (lst n)
+ (if lst
+  (if (zerop n)
+   (cons nil (cons lst nil))
+   ((lambda (elem result)
+     (cons
+      (cons elem (car result))
+      (cdr result)))
+    (car lst)
+    (task (cdr lst) (1- n))))))
+
+(print(task '(a b c d e f g) 3))
+(print(task '(a b c d e f g) 1))
+(print(task '(a b c d e f g) 0))
+
+
 ; 5: Oпределите функцию, которая увеличивает элементы исходного списка на единицy
 
 (defun inc-list (lst) (
@@ -28,27 +65,6 @@
 (print(del-even '(1)))
 (print(del-even '())) 
 
-; 12:  Определите функцию, заменяющую в исходном списке два подряд идущих одинаковых элемента одним
-
-(defun task (lst) (
-                   cond ((null lst) NIL)
-                   ((equal (car lst) 
-                           (cadr lst)
-                           ) 
-                    (cons (car lst) 
-                          (task (cddr lst))
-                          )
-                    )
-                   (t (cons (car lst) 
-                            (task (cdr lst)))
-                      )
-                   )
-  )
-
-(print(task '(1 1 1 2 2 2))) 
-(print(task '(1 2 3)))
-(print(task '(2 2 2 2 2 2 2 3))) 
-
 ;15: Определите функцию, вычисляющую скалярное произведение векторов, заданных списками целых чисел.
 
 (defun scal-prod (lst1 lst2)
@@ -70,24 +86,5 @@
 
 
 ; 13. Определите функцию, удаляющую в исходном списке все повторные вхождения элементов.
-
-
-
-; 11. Определите функцию, осуществляющую разделение исходного списка на два подсписка. В первый из них должно попасть указанное количество элементов с начала списка, во второй — оставшиеся элементы.
-
-(defun task (lst n)
- (if lst
-  (if (zerop n)
-   (cons nil (cons lst nil))
-   ((lambda (elem result)
-     (cons
-      (cons elem (car result))
-      (cdr result)))
-    (car lst)
-    (task (cdr lst) (1- n))))))
-
-(print(task '(a b c d e f g) 3))
-(print(task '(a b c d e f g) 1))
-(print(task '(a b c d e f g) 0))
 
 ;31. Определите функцию (ПЕРВЫЙ-СОВПАДАЮЩИЙ х у), которая возвращает первый элемент, входящий в оба списка х и у, в противном случае NIL.
